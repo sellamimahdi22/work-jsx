ac39b9f4915cc141875471d3b811085b8bdccbcf
sellamimahdi22 <sellamimahdi.ms23@gmail.com>
2021-11-17 11:34:04 +0100

    fgdgdyt

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import x from './imagesrc.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <img src={x} alt ='myImage' />
+        <img src="image.jpg" alt ='myImage' />
+    </div>
+  );
+}
+
+export default App;
